f75fdcf0531fc04cc4b6df66a9e40e7fd1004515
Jhonny Perez <jhonnyjose1998@gmail.com>
2018-11-04 14:18:17 -0430

    Inicio de la practica, manipular objetos y arrays en js

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// document.getElementById('btn')
+// 		.addEventListener('hacerAlgo');
+
+// Creanado un array
+var array=['jhonny','jose','perez','martinez'];
+// Añadiendo mas datos al array
+array.push('Hola','Chao','Arigato','Coño');
+
+function hacerAlgo(){
+	// Mostrando el array en la posicion 7
+	// alert('hola '+array[7]);
+	// Creando un objeto y mostrando todo su contenido en formato json por medio de un alert
+	var Objeto={name:"Jhonny",age:20,mail:"jhonnyjose1998@gmail.com"};
+	// añadiendo un objeto al array
+	array.push(Objeto);
+	// alert(JSON.stringify(Objeto));
+	// accediendo y mostrando un objeto contenido en un array por medio de un alert
+	alert(array[8].mail);
+
+}
+
+// Llamando a la funcion
+hacerAlgo();
